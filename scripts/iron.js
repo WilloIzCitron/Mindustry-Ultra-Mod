ce1d9a8e3458f6cd0a353f86413ce69ed42dd8cf
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2020-11-18 07:46:26 +0700

    Changes

---
+++ scripts/iron.js
@@ -0,0 +1,3 @@
+const irons = new Item("iron", Color.valueOf("b0bac0"));
+irons.cost = 2;
+irons.hardness = 5;
---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+require("testunit4");
+require("activationmatter-weaver");
+require("multi-cultivator");
+require("atmospheric-cooler");
+//require("iron");
+//require("ore-iron");
+require("test");
+//require("techtree");
+require("page");
+require("power-processor");
+require("impact-projector");
+
+/*
+credit({
+  younggam: "answering tons of question",
+  deltaNedas: "refer to routorio, rtfm",
+  sk7725: "answering tons of question, refer to testers"
+*/
