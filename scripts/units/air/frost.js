cc3f83501d596ad2b1e80593248210847e512494
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2020-11-22 07:33:33 +0700

    add splash dmg and nerf a frost's building time

--- scripts/units/air/frost.js
+++ scripts/units/air/frost.js
@@ -1,4 +1,4 @@
 const GlacierAirT1 = extendContent(UnitType, "frost", {});
 GlacierAirT1.ammotype = AmmoTypes.copper;
 GlacierAirT1.constructor = () => extend(UnitEntity, {});
-Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(GlacierAirT1, 60 * 25, ItemStack.with(Items.silicon, 45, Items.lead, 20, Items.metaglass, 25)));
+Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(GlacierAirT1, 60 * 15, ItemStack.with(Items.silicon, 45, Items.lead, 20, Items.metaglass, 25)));
