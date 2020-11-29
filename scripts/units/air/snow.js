b0870068cc8c5de6c5d0bb2d51b40c996727efba
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2020-11-29 07:07:48 +0700

    Update snow.js

--- scripts/units/air/snow.js
+++ scripts/units/air/snow.js
@@ -2,5 +2,5 @@ const GlacierAirT2 = extendContent(UnitType, "snow", {});
 GlacierAirT2.ammotype = AmmoTypes.copper;
 GlacierAirT2.constructor = () => extend(UnitEntity, {});
 
-var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "frost"), Vars.content.getByName(ContentType.unit, "snow")]);
+var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "mindustry_ultra_mod-frost"), Vars.content.getByName(ContentType.unit, "mindustry_ultra_mod-snow")]);
 Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
